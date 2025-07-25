24045181819f65e625d8e46000b5f1e71c74dc3f
Kenj <122892187+kjyyx@users.noreply.github.com>
2025-07-25 18:23:51 +0800

    Merge remote changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-});
+  plugins: [react()],
+  base: '/Kyzen/', // This should match your repository name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
+})
